78300a98b59d23c9ba292280b6cb93f8d907b51b
arya_0091 <arypatel913@gmail.com>
2025-09-12 19:25:17 +0000

    basiz

---
+++ 01_basics/Datatype.js
@@ -0,0 +1,11 @@
+"use strict"//threat all js code new version
+
+console.log(3+3)
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false;
+let state;
+
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 01_basics/variable.js
@@ -0,0 +1,14 @@
+const accountId=12334
+let accountEmail="ary123@122"
+var accountPassword="12334"
+accountCity="ahemdabad"
+
+let accountState;
+
+accountEmail="ary9123"
+var accountPassword="2173183218321"
+
+console.log(accountEmail);
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword])
