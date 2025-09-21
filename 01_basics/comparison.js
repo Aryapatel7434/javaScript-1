03d8f9f4195167300df132e49c4ffe2974b0fcc8
arya_0091 <arypatel913@gmail.com>
2025-09-21 13:57:25 +0000

    Y

---
+++ 01_basics/comparison.js
@@ -0,0 +1,21 @@
+//  console.log(2 > 1);
+//  console.log(2 >= 1);
+//  console.log(2 < 1);
+//  console.log(2 == 1);
+//  console.log(2 != 1);
+
+
+// console.log("2" > 1);//avoid this type comparison please compare the same data type 
+//  console.log("02" > 1);
+
+console.log(null > 0);//null consider 0 and undefined
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);//=== check strickly even data type check
