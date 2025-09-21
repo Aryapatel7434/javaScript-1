3fe1f59a509c6350f3af9cdeda86dd70f438021d
arya_0091 <arypatel913@gmail.com>
2025-09-21 12:56:27 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,25 @@
+let score=null
+console.log(typeof(score));
+
+let valuenumber=Number(score);
+console.log(typeof(valuenumber));
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let score1="33abc"
+let valuein=Number(score1)
+console.log(typeof(score1));
+console.log(valuein);
+//NAN means Not a Number
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+ let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
