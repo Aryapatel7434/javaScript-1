ce05a0ec242e3b98ab23056d278f098a51293991
arya_0091 <arypatel913@gmail.com>
2025-09-21 13:35:42 +0000

    X

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -47,3 +47,14 @@ console.log(1+2+"2");
 
   console.log(+true);
  console.log(+"");
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
