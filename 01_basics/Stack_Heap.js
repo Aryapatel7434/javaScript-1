e81e7127a1c23d2a54784748fdaa432824d5c2be
arya_0091 <arypatel913@gmail.com>
2025-09-22 05:13:14 +0000

    Stack_Heap

---
+++ 01_basics/Stack_Heap.js
@@ -0,0 +1,22 @@
+//Stack(Premitive),Heap(Non premitive)
+
+let myYoutubename="arypatel1@gamil.com"
+
+let anothername=myYoutubename  //only copy not change in original data
+
+anothername="arypatel"
+
+console.log(anothername);
+console.log(myYoutubename);
+
+//object
+let userOne={
+    email:"arypatel22@gmail.com",
+    upi:"@ybl",
+}
+
+let userTwo=userOne
+
+userTwo.email="arypatel23@gamil.com"//change is original value .
+
+console.log(userOne.email);
